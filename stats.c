9cf47ea0c3d176133b665f87811067ef06313496
ChitraNallasamy <91539731+ChitraNallasamy@users.noreply.github.com>
2021-10-25 17:26:55 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,4 +1,5 @@
 #include "stats.h"
+#include "math.h"
 
 Stats compute_statistics(const float* numberset, int setlength) {
     Stats s;
@@ -8,28 +9,35 @@ Stats compute_statistics(const float* numberset, int setlength) {
      s.max = numberset[0];
      s.min = numberset[0];
      sum = numberset[0];
-     
-    for (i=1; i<setlength; i++)
-    {
-      if (numberset[i]> s.max)
-      {
-          s.max=numberset[i];
-      }
-      else if (numberset[i]<s.min)
-      {
-         s.min=numberset[i];
-      }
-      else
-      {
-          /* do nothing */
-      }    
-      sum += numberset[i];
-    }
-    s.average =(float)sum/setlength;
     
+    if((numberset != NULL) && (setlength != 0))
+    {       
+        for (i=1; i<setlength; i++)
+        {
+          if (numberset[i]> s.max)
+          {
+              s.max=numberset[i];
+          }
+          else if (numberset[i]<s.min)
+          {
+             s.min=numberset[i];
+          }
+          else
+          {
+              /* do nothing */
+          }    
+          sum += numberset[i];
+        }
+        s.average =(float)sum/setlength;
+    }
+    else
+    {
+        s.max = NAN;
+        s.min = NAN;
+        s.average = NAN;
+    }
     return s;
 }
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-

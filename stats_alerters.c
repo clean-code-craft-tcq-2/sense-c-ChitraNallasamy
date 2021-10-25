4241752ad06b9d0bcc52e544d48e01f73fc3b763
ChitraNallasamy <91539731+ChitraNallasamy@users.noreply.github.com>
2021-10-25 18:05:16 +0530

    Update stats_alerters.c

--- stats_alerters.c
+++ stats_alerters.c
@@ -1,1 +1,20 @@
+#include "stats.h"
 
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats)
+{
+  if(computedStats.max > maxThreshold)
+  {
+    (*alerter_funcptr[0])();
+    (*alerter_funcptr[1])();
+  }
+}
+
+void emailAlerter(void)
+{
+   emailAlertCallCount = 1;
+}
+
+void ledAlerter(void)
+{
+   ledAlertCallCount = 1;
+}
